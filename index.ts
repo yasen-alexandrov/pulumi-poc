0eb84e9f8f8943d250d8590f8b976fd581a1fcab
Yasen Aleksandrov <y.aleksandrov@prosyst.bg>
2022-01-05 10:40:10 +0200

    changed name of sa once

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import * as storage from "@pulumi/azure-native/storage";
 const resourceGroup = new resources.ResourceGroup("rgPulumiStateInAzure",{location: "West Europe"});
 
 // Create an Azure resource (Storage Account)
-const storageAccount = new storage.StorageAccount("sadevelopbranch1", {
+const storageAccount = new storage.StorageAccount("sadevelopbranch", {
     resourceGroupName: resourceGroup.name,
     sku: {
         name: storage.SkuName.Standard_LRS,
